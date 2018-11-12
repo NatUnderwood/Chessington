a4521489f36afa926ccd8a567bb2e94c5d535fb2
Natalie Underwood <Natalie.Underwood@softwire.com>
2018-11-12 15:49:42 +0000

    knight can move in an L

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,7 +11,40 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var movesList = new List<Square>();
+            var location = board.FindPiece(this);
+            if (checkIfAllowedSquare(location.Row + 1, location.Col + 2))
+                movesList.Add(Square.At(location.Row + 1, location.Col+2));
+
+            if (checkIfAllowedSquare(location.Row - 1, location.Col + 2))
+                movesList.Add(Square.At(location.Row - 1, location.Col + 2));
+
+            if (checkIfAllowedSquare(location.Row - 1, location.Col - 2))
+                movesList.Add(Square.At(location.Row - 1, location.Col - 2));
+
+            if (checkIfAllowedSquare(location.Row + 1, location.Col - 2))
+                movesList.Add(Square.At(location.Row + 1, location.Col - 2));
+
+            if (checkIfAllowedSquare(location.Row - 2, location.Col + 1))
+                movesList.Add(Square.At(location.Row - 2, location.Col + 1));
+
+            if (checkIfAllowedSquare(location.Row - 2, location.Col - 1))
+                movesList.Add(Square.At(location.Row - 2, location.Col - 1));
+
+            if (checkIfAllowedSquare(location.Row + 2, location.Col - 1))
+                movesList.Add(Square.At(location.Row + 2, location.Col - 1));
+
+            if (checkIfAllowedSquare(location.Row + 2, location.Col + 1))
+                movesList.Add(Square.At(location.Row + 2, location.Col + 1));
+
+            return movesList;
+        }
+
+        public bool checkIfAllowedSquare( int attemptedRow, int attemptedCol)
+        {
+            if (attemptedCol < 8 && attemptedCol >= 0 && attemptedRow < 8 && attemptedRow >= 0)
+                return true;
+            return false;
         }
     }
 }
