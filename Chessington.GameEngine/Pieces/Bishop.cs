a2cb10f97a9cdd5bda0f451aba539169227662f0
Natalie Underwood <Natalie.Underwood@softwire.com>
2018-11-12 15:23:01 +0000

    bishop moves

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,7 +12,30 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var movesList = new List<Square>();
+            var location = board.FindPiece(this);
+            var max = Math.Max(location.Row, location.Col);
+            var min = Math.Min(location.Row, location.Col);
+            var minMoves = Math.Min(8 - max, min);
+            for (var i=1;i<(8-max);i++)
+            {
+                movesList.Add(Square.At(location.Row+i,location.Col+i));
+            }
+            for (var i = 1; i < min+1; i++)
+            {
+                movesList.Add(Square.At(location.Row - i, location.Col - i));
+            }
+            for (var i = 1; i <  minMoves; i++)
+            {
+                movesList.Add(Square.At(location.Row - i, location.Col + i));
+            }
+            for (var i = 1; i < minMoves; i++)
+            {
+                movesList.Add(Square.At(location.Row + i, location.Col - i));
+            }
+
+            return movesList;
         }
+
     }
 }
